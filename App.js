9f91673504e5062a22584b85e58fd639bc28500c
karlaevelize <k.evelize@gmail.com>
2021-09-30 11:27:53 +0200

    Example from morning lecture

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+import { ScrollView, Text, Image, Button, Alert, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export default function App() {
+
+  const [ data, setData ] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get("https://hp-assessment-api.herokuapp.com/characters")
+    setData(response.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text style={{marginVertical: 45, fontSize: 27, fontWeight: "bold", textAlign: "center"}}>
+        Hello from React Native
+      </Text>
+      {/* what if I want to click the image? */}
+      {/* use "TouchableOpacity" to make all clickable */}
+      <TouchableOpacity onPress={() => Alert.alert("Don't push me!")}>
+        {/* needs a source and style to show up depending on size */}
+        <Image 
+          source={{uri: "https://i.ytimg.com/vi/lohz29YN3mA/maxresdefault.jpg"}}
+          style={{width: "100%", height: 100}}
+        />
+      </TouchableOpacity>
+      {/* you can use alert.alert or alert.prompt */}
+      {/* prompt get input from user */}
+      <Button 
+        onPress={() => Alert.alert("Yaaaaa!")} 
+        title="Press me" 
+        color="hotpink"
+      />
+      {data.map(char => {
+        return (
+          <View>
+            <Text style={{textAlign: "center", marginVertical: 5, fontSize: 20, fontWeight: "bold"}}>{char.name}</Text>
+            <Image 
+              source={{uri: char.imgUrl}}
+              style={styles.image}
+            />
+          </View>
+        )
+      })}
+    </ScrollView>
   );
 }
 
+// cleaner of styling, then you can reuse the code
+// prevents a lot of inline styling
+// check line 45 for usage
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width: 100, 
+    height: 100, 
+    marginHorizontal: 150
   },
-});
+})
